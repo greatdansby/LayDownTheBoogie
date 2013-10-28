09727062ddaf9fa232bf985ebc0383f4182b0355
Daniel Mitus <danielmitus@gmail.com>
2013-10-28 18:34:17 -0400

    Songlist format

--- Template/SongLists.php
+++ Template/SongLists.php
@@ -1,4 +1,92 @@
+<?php  
 
+//connect to the database 
+$con = mysqli_connect('localhost', 'boogie','boogie6', 'boogie_requests');
+if (!$con)
+  {
+  die('Could not connect: ' . mysqli_error());
+  } 
+$loggedin=false;
+$path = getcwd();
+$dj =  trim(substr($path,strrpos($path,"/")-strlen($path)+1));
+$songlist = $_POST['songlist'];
+if(isset($_POST['pw'])){
+$pw = $_POST['pw'];
+$sql = "SELECT DJ FROM DJs WHERE DJ='$dj' AND PW='".$pw."'";
+$result = mysqli_query($con,$sql);
+if(mysqli_num_rows($result)==1){
+$loggedin=true;
+$sql = "UPDATE DJs SET LastIP = '".$_SERVER['REMOTE_ADDR']."' WHERE DJ='$dj'";
+if(!mysqli_query($con,$sql)){printf("Error: %s\n", mysqli_error($con));}
+$sql="SELECT ListName, ShowGenre, AvailableList, SongCount FROM SongLists WHERE DJ = '$dj' Order By ListName";
+$songlists = loadArray(mysqli_query($con, $sql),array('ListName', 'ShowGenre', 'AvailableList', 'SongCount'));
+} else {
+echo "<div class='alert alert-block alert-danger fade in'>
+        <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>×</button>
+        <h4>Password incorrect</h4>
+        <p>Please check your password and try again.</p>
+      </div>";}
+}else {
+$sql = "SELECT DJ FROM DJs WHERE DJ='$dj' AND LastIP='".$_SERVER['REMOTE_ADDR']."'";
+$result = mysqli_query($con,$sql);
+if(mysqli_num_rows($result)==1){
+$loggedin=true;
+/*echo "<div class='alert alert-block alert-info fade in'>
+        <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>×</button>
+        <h4>Please enter your password above</h4>
+        <p>E-mail info@LayDownTheBoogie.com if you have any issues or need to reset your password.</p>
+      </div>";*/
+}}
+
+if ($_FILES[csv][size] > 0) { 
+
+    //echo "get the csv file"."<br>"; 
+    $file = $_FILES[csv][tmp_name]; 
+    $handle = fopen($file,"r"); 
+    //$data = fgetcsv($handle,1000,",","'");
+    //loop through the csv file and insert into database 
+    do { 
+        if ($data[0]) { 
+			//echo "1 row inserted";
+            $sql=("INSERT INTO CustomLists (DJ, SongList, SongTitle, SongArtist, SongGenre, Status) VALUES 
+                ( 
+                    '".$dj."',
+					'".$songlist."',
+					'".addslashes($data[0])."', 
+                    '".addslashes($data[1])."', 
+                    '".addslashes($data[2])."',
+					'Active'				
+                ) 
+            "); 
+			//echo $sql;
+			if(!mysqli_query($con,$sql)){printf("Error: %s\n", mysqli_error($con));}
+        } 
+    } while ($data = fgetcsv($handle,1000,",","'")); 
+    // 
+	header('Location: SongLists.php?success=1'); die; 
+}
+
+function loadArray($result,$columns){
+	$rows = array();
+	if(count($columns)>1){
+		$i=0;
+		while($r = mysqli_fetch_assoc($result)) {
+			$rows[$i] = array();
+			foreach($columns as $key){
+				$rows[$i][$key] = $r[$key];
+			}
+			$i++;
+		}
+	} else {
+		while($r = mysqli_fetch_assoc($result)) {
+			foreach($columns as $key){
+				$rows[] = $r[$key];
+			}
+		}
+	}
+	return $rows;
+}
+?>
 <html>
 	<head>
 		<title>Lay Down the Boogie - <?echo $dj;?></title>
@@ -32,7 +120,16 @@
 	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 	  <div class="navbar-header">
 		<a class="navbar-brand" href="#">DJ Dashboard</a>
-		
+		<?php if($loggedin){
+		echo '<div class="nav navbar-nav">'.$dj.'</div>';
+		} else {
+		echo '
+		<form class="navbar-form navbar-left" role="search" method=post action=SongLists.php id="frm1">
+			<div class="form-group">
+				<input type="password" name="pw" class="form-control" placeholder="Password">
+			</div>
+			<button type="submit" class="btn btn-default" name="login" id="login">Login</button>
+		</form>';}?>
 		<ul class="nav navbar-nav">
 			<li><a href="DJDashboard.php">Requested Songs</a></li>
 			<li class="active"><a href="#">Song Lists & Settings</a></li>
@@ -40,7 +137,9 @@
 	  </div>
 	</nav>
     <div class="container" style="margin-top:40px;" data-toggle="collapse">
-
+		<?php if (!empty($_GET[success])) { echo "<div class='alert alert-block alert-info fade in'>
+        <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>×</button>
+        <h4>Your file has been uploaded successfully.</h4></div>"; } //generic success notice ?> 
 		<div class="col-xs-12 col-md-6">
 			<div class="col-xs-12 col-md-12">
 				<h1 class=white>Upload Custom Artwork</h1>
