63fd6229e03253a2326d63f7a5080b9fec6e0cf1
Daniel Mitus <danielmitus@gmail.com>
2013-11-10 19:16:37 -0500

    Update

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 				}
 			</style>
 	<body>
-	<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="navbar navbar-fixed-top">
       <div class="container nav-style">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
