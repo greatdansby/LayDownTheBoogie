d49a560efa9d4bced703e82e73ffaa3cab938fd0
Daniel Mitus <danielmitus@gmail.com>
2013-12-06 19:36:55 -0500

    login fix

--- Template/djdashboard.php
+++ Template/djdashboard.php
@@ -108,7 +108,7 @@ function loadArray($result,$columns){
       <div class="container nav-style">
 		  <div class="navbar-header">
 			<a class="navbar-brand" href="#">DJ Dashboard
-			<?php if($loggedin){
+			<?php if($loggedin==true){
 			echo ': '.$dj.'</a>';
 			} else {
 			echo '</a>
